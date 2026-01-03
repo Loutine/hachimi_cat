402fb69157e612ad2bc955025c0d5805aa56af61
Loutine <uhuru-loutine@outlook.com>
2026-01-04 06:38:08 +0800

    refactor: clean up nix files, and add a new rustc target

---
+++ nix/devShell.nix
@@ -0,0 +1,62 @@
+{ pkgs, lib, ... }:
+let
+  nix-tools = with pkgs; [
+    nil
+    nixd
+    nixfmt-rfc-style
+  ];
+  wasm-tools = with pkgs; [
+    wasm-bindgen-cli
+    wasm-pack
+  ];
+  build-tools = with pkgs; [
+    pkg-config
+    autoconf
+    automake
+    cmake
+  ];
+  extraPackages =
+    with pkgs;
+    [
+      libclang
+      webrtc-audio-processing
+      libopus
+    ]
+    ++ lib.optionals stdenv.isLinux [
+      pkgs.libtool
+      pkgs.alsa-lib.dev
+    ]
+    ++ lib.optionals stdenv.isDarwin [
+      pkgs.glibtool
+    ];
+in
+{
+  languages.rust = {
+    enable = true;
+    channel = "stable";
+    components = [
+      "rustc"
+      "rust-src"
+      "cargo"
+      "clippy"
+      "rustfmt"
+      "rust-analyzer"
+    ];
+    targets = [
+      "wasm32-unknown-unknown"
+      "x86_64-unknown-linux-gnu"
+      "aarch64-apple-darwin"
+    ];
+  };
+
+  languages.javascript = {
+    enable = true;
+    npm.enable = true;
+    yarn.enable = true;
+  };
+
+  packages = wasm-tools ++ build-tools ++ nix-tools ++ extraPackages;
+
+  env.LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+
+}
